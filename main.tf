f838ee64794f88bd02d6cafe8cdc1d4e1df3c60e
Shahid Karim <skarim759@gmail.com>
2025-12-23 22:43:33 +0545

    Initial commit: ec2+s3 Terraform project

---
+++ main.tf
@@ -0,0 +1,215 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# using latest ami
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-kernel-6.1-x86_64"]
+  }
+}
+
+# using existing vpc
+data "aws_vpc" "default_vpc" {
+  id = "vpc-04b9661aff908e210"
+}
+
+# Using existing subnet
+data "aws_subnet" "default_subnet" {
+  id = "subnet-0591baba4adf985b9"
+}
+
+# Using existing secuirty group
+data "aws_security_group" "default_sg" {
+  id = "sg-05dbbd278e09a6b2b"
+}
+
+##################################################################
+####################### EC2 Portion ##############################
+##################################################################
+
+
+######### main ec2 resource ##############
+resource "aws_instance" "shahid_ec2" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t3.micro"
+  key_name               = "awsKeyPair"
+  subnet_id              = data.aws_subnet.default_subnet.id
+  vpc_security_group_ids = [data.aws_security_group.default_sg.id]
+
+  tags = {
+    Name      = "terraform-shahid-ec2"
+    ManagedBy = "Terraform"
+  }
+
+}
+
+######### Attaching EIP to the instance #############
+resource "aws_eip" "shahid_eip" {
+  instance = aws_instance.shahid_ec2.id
+}
+
+##################################################################
+################ S3 bucket starts from here ######################
+##################################################################
+
+
+########### Log Bucket ###########
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "secure-bucket-logs-karim-2025-12-23"
+
+  tags = {
+    Name      = "S3-Logs"
+    ManagedBy = "Terraform"
+  }
+}
+
+# Object Ownership
+resource "aws_s3_bucket_ownership_controls" "log_bucket" {
+  bucket = aws_s3_bucket.log_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+
+}
+
+# Block Public Access settings for this bucket
+resource "aws_s3_bucket_public_access_block" "log_bucket" {
+  bucket = aws_s3_bucket.log_bucket.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
+
+# Bucket Versioning
+resource "aws_s3_bucket_versioning" "log_bucket" {
+  bucket = aws_s3_bucket.log_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "log_bucket" {
+  bucket = aws_s3_bucket.log_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+
+########### Secure Bucket ###########
+resource "aws_s3_bucket" "secure_bucket" {
+  bucket = "secure-bucket-karim-2025-12-23"
+
+  tags = {
+    Name      = "S3-Secure"
+    ManagedBy = "Terraform"
+  }
+}
+
+# Server access logging
+resource "aws_s3_bucket_logging" "secure_bucket" {
+  bucket        = aws_s3_bucket.secure_bucket.id
+  target_bucket = aws_s3_bucket.log_bucket.id
+  target_prefix = "access-logs/"
+}
+
+# Object Ownership
+resource "aws_s3_bucket_ownership_controls" "secure_bucket" {
+  bucket = aws_s3_bucket.secure_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+
+}
+
+# Block Public Access settings for this bucket
+resource "aws_s3_bucket_public_access_block" "secure_bucket" {
+  bucket = aws_s3_bucket.secure_bucket.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
+
+# Bucket Versioning
+resource "aws_s3_bucket_versioning" "secure_bucket" {
+  bucket = aws_s3_bucket.secure_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "secure_bucket" {
+  bucket = aws_s3_bucket.secure_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Bucket Policy
+resource "aws_s3_bucket_policy" "secure_bucket" {
+  bucket = aws_s3_bucket.secure_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "DenyInsecureTransport"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource = [
+          aws_s3_bucket.secure_bucket.arn,
+          "${aws_s3_bucket.secure_bucket.arn}/*"
+        ]
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
+      },
+      {
+        Sid       = "DenyUnencryptedObjectUploads"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:PutObject"
+        Resource  = "${aws_s3_bucket.secure_bucket.arn}/*"
+        Condition = {
+          StringNotEquals = {
+            "s3:x-amz-server-side-encryption" : "AES256"
+          }
+        }
+      }
+    ]
+  })
+}
